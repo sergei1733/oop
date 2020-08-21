02a9924b098fa4270dd15333c307c196587832a7
Sergei <sergei1733@yandex.ru>
2020-08-21 17:50:24 +0500

    end 3 task

--- src/OOP3/Main.java
+++ src/OOP3/Main.java
@@ -1,4 +1,17 @@
 package OOP3;
 
 public class Main {
+    public static void main(String[] args) {
+        Matrix p1 = new Matrix(3,3);
+        p1.zapolnenieMass();
+        p1.printRealNumbers();
+
+        p1.multiplicationByNumber(2);
+        p1.printRealNumbers();
+
+        Matrix p2 = new Matrix(3,3);
+        p2.zapolnenieMass();
+
+        p1.matrixAddition(p2);
+    }
 }
--- src/OOP3/Matrix.java
+++ src/OOP3/Matrix.java
@@ -1,4 +1,42 @@
 package OOP3;
 
 public class Matrix {
+    private int row;
+    private int column;
+    private int[][] realNumbers;
+
+    public Matrix(int row, int column){
+        this.realNumbers= new int[row][column];
+    }
+
+    public void zapolnenieMass(){
+        for (int i = 0; i <this.realNumbers.length ; i++) {
+            for (int j = 0; j <this.realNumbers[i].length ; j++) {
+                this.realNumbers[i][j] = 15;
+            }
+        }
+    }
+
+    public void printRealNumbers(){
+        for (int i = 0; i <this.realNumbers.length ; i++) {
+            for (int j = 0; j <this.realNumbers[i].length ; j++) {
+                System.out.print(this.realNumbers[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public void multiplicationByNumber(int number){
+        for (int i = 0; i <this.realNumbers.length ; i++) {
+            for (int j = 0; j <this.realNumbers[i].length ; j++) {
+                this.realNumbers[i][j]*=number;
+            }
+        }
+    }
+    public void matrixAddition(int matrix[][]){
+        for (int i = 0; i <this.realNumbers.length ; i++) {
+            for (int j = 0; j <this.realNumbers[i].length ; j++) {
+                this.realNumbers[i][j]+=matrix[i][j];
+            }
+        }
+    }
 }
