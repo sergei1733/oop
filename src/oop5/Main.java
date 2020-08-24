cb2b05bb7b9ed7515ed673856c0c6d9413766a87
Sergei <sergei1733@yandex.ru>
2020-08-24 23:05:59 +0500

    end 5 task

---
+++ src/oop5/Main.java
@@ -0,0 +1,26 @@
+package oop5;
+
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+    int a,b;
+    a = in.nextInt();
+    b = in.nextInt();
+        numbers(a, b);
+    }
+    private static void numbers(int a, int b){
+        if (a<b) {
+            System.out.print(a + " ");
+            if (a != b)
+                numbers(a + 1, b);
+        }
+
+       if (b<a) {
+            System.out.print(b + " ");
+            if (b != a)
+                numbers(a, b + 1);
+        }
+    }
+}
