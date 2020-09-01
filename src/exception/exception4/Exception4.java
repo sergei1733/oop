f36a6064db7c0b0d341f4bce774818e78ec3d662
Sergei <sergei1733@yandex.ru>
2020-09-01 18:23:46 +0500

    модификаторы доступа

---
+++ src/exception/exception4/Exception4.java
@@ -0,0 +1,19 @@
+package exception.exception4;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+public class Exception4 {
+    public static void main(String[] args) {
+        try {
+            run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void run () throws IOException, ParseException, IllegalAccessError {
+
+    }
+}
